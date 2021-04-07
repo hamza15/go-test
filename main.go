6c398260210d215f8a5dd9124c432d254748b424
Hamza Naqi <sardan5@vt.edu>
2021-04-07 08:41:15 -0400

    test addition

--- main.go
+++ main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
 	w.Write([]byte("Hello World"))
 }
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux := mux.NewRouter()
+	mux.HandleFunc("/", home).Methods("GET")
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
